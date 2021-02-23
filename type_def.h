d444d75ef9cf7367a6ab75a703814ca6084dfa9a
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-02-23 23:05:09 +0530

    Keeping datatype definition

--- type_def.h
+++ type_def.h
@@ -2,13 +2,6 @@ enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
 
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
