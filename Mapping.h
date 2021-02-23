e473a4bf5f194bb7ac68ab99750bf1e4d07aabd3
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-02-23 23:30:49 +0530

    Update Mapping.h

--- Mapping.h
+++ Mapping.h
@@ -1,5 +1,7 @@
 #include "type_def.h"
 
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 
 ColorPair GetColorFromPairNumber(int pairNumber); 
