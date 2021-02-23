ab4459d69ed4076d746657b9c5a2ad1cc4718b30
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-02-23 23:07:21 +0530

    Update Mapping.c

--- Mapping.c
+++ Mapping.c
@@ -1,6 +1,19 @@
 #include "Mapping.h"
 #include <stdio.h>
 
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
