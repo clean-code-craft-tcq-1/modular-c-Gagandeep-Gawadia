27e23a798f426764a77d390d28daccf0579b4cd8
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-02-25 09:40:24 +0530

    Update test and function names

--- main.c
+++ main.c
@@ -4,19 +4,19 @@
 #include <assert.h>
 
 
-void testNumberToPair(int colorCode,
+void test_MappedColorPair4code(int colorCode,
     enum MajorColor expectedMajorColor,
     enum MinorColor expectedMinorColor)
 {
-    ColorPair mappedColorPair = GetColorFromPairNumber(colorCode);
+    ColorPair mappedColorPair = MappedColorPair4code(colorCode);
     char mappedColorPairString[MAX_COLORPAIR_NAME_CHARS];
-    ColorPairToString(&mappedColorPair, mappedColorPairString);
+    FormatColorPair2String(&mappedColorPair, mappedColorPairString);
     printf("For the provided color code %d, the corresponding color pair (Major:Minor) is : %s\n", colorCode, mappedColorPairString);
     assert(mappedColorPair.majorColor == expectedMajorColor);
     assert(mappedColorPair.minorColor == expectedMinorColor);
 }
 
-void testPairToNumber(
+void test_MappedCode4ColorPair(
     enum MajorColor majorColor,
     enum MinorColor minorColor,
     int expectedColorCode)
@@ -24,17 +24,17 @@ void testPairToNumber(
     ColorPair colorPair;
     colorPair.majorColor = majorColor;
     colorPair.minorColor = minorColor;
-    int mappedColorCode = GetPairNumberFromColor(&colorPair);
+    int mappedColorCode = MappedCode4ColorPair(&colorPair);
     printf("For the given color pair the corresponding color code is %d\n", mappedColorCode);
     assert(mappedColorCode == expectedColorCode);
 }
 
 int main() {
-    testNumberToPair(4, WHITE, BROWN);
-    testNumberToPair(5, WHITE, SLATE);
+    MappedColorPair4code(4, WHITE, BROWN);
+    MappedColorPair4code(5, WHITE, SLATE);
 
-    testPairToNumber(BLACK, ORANGE, 12);
-    testPairToNumber(VIOLET, SLATE, 25);
+    MappedCode4ColorPair(BLACK, ORANGE, 12);
+    MappedCode4ColorPair(VIOLET, SLATE, 25);
     
     // Print entire 25 color code manual for reference
     PrintManual();
